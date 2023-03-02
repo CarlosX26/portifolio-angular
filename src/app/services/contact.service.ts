c196ca41b2922269d2af0cad744479499ff9ba38
CarlosX26 <cjunior1@live.com>
2023-03-02 15:42:36 -0300

    feat: add request for api contact

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export interface IContactInfo {
+  name: string;
+  tel: string;
+  email: string;
+}
+
+interface IResponseContactInfos {
+  subject_matter: string;
+  comment: string;
+  contact: IContactInfo;
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class ContactService {
-  constructor() {}
+  baseUrl: string =
+    "https://63a59f6af8f3f6d4abfb383d.mockapi.io/api-portfolio/sendEmail";
+
+  constructor(private http: HttpClient) {}
+
+  getContactInfos(): Observable<IResponseContactInfos> {
+    return this.http.get<IResponseContactInfos>(this.baseUrl);
+  }
 }
